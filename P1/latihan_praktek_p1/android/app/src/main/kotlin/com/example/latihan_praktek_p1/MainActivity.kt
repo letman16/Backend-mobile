4e2a06d7d50a598cb68ac683474e09220495dda8
Lukman Hakim <letmantech@gmail.com>
2023-09-30 13:31:21 +0700

    fix 1.0

--- P1/latihan_praktek_p1/android/app/src/main/kotlin/com/example/latihan_praktek_p1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.latihan_praktek_p1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- P2/latihan_praktek_2/android/app/src/main/kotlin/com/example/latihan_praktek_2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.latihan_praktek_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
