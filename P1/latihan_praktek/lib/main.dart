8cc6aed5d35c8a5006688387df2b09307c9b7ba4
Lukman Hakim <letmantech@gmail.com>
2023-09-30 17:02:32 +0700

    fixing P1, masih ada bug

--- P1/latihan_praktek/lib/main.dart
+++ P1/latihan_praktek/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:latihan_praktek/Contoh2.dart';
-import 'package:latihan_praktek/DaftarPengguna.dart';
 
 void main() {
   runApp(const MyApp());
